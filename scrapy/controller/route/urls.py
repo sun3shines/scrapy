7bb026eb0feaac722994d806a0a9c331fec303c9
sun3shines <sun__shines@163.com>
2016-03-07 06:00:05 +0800

    frame done.parse thread turn

--- scrapy/controller/route/urls.py
+++ scrapy/controller/route/urls.py
@@ -4,9 +4,11 @@ from cloudcommon.common.bufferedhttp import jresponse
 from scrapy.controller.views.url import dispatch
 from scrapy.controller.views.host import remove
 import scrapy.controller.views.config
+from scrapy.center.link import urlmerge
+from scrapy.center.proxy import proxymerge
 
 url2view = {}
-strUrlPutGet = '/url/put/get'
+strUrlPutGet = '/url/dispatch'
 url2view.update({strUrlPutGet:dispatch})
 
 strHostRemove = '/host/remove'
@@ -14,6 +16,12 @@ url2view.update({strHostRemove:remove})
 
 strConfigGet = '/config/get'
 url2view.update({strConfigGet:scrapy.controller.views.config.get})
+
+strUrlMerge = '/url/merge'
+url2view.update({strUrlMerge:urlmerge})
+
+strProxyMerge = '/proxy/merge'
+url2view.update({strProxyMerge:proxymerge})
 
 def handlerequest(req):
     
