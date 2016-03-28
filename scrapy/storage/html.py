58bafefde1119040e0fb4d397d9727b334230b2f
root <root@lab.(none)>
2016-03-29 06:37:33 +0800

    start

---
+++ scrapy/storage/html.py
@@ -0,0 +1,9 @@
+
+class Html:
+
+    def __init__(self,url,article):
+        pass
+
+    def save(self):
+        pass
+
---
+++ scrapy/storage/url.py
@@ -0,0 +1,9 @@
+
+class Url:
+    def __init__(self,url):
+        self.url = url
+
+    def save(self):
+        pass
+
+
